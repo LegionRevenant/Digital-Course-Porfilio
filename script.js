fb367e53df521740464d3a57a68ca1e91d14e750
LegionRevenant <danbap300@gmail.com>
2025-01-31 11:16:41 +0800

    `Updated index.html, script.js, and styles.css with minor changes to layout, images, and modal functionality.`

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Display the success message
-    document.getElementById('successMessage').style.display = 'block';
-});
+var modal = document.getElementById("imageModal");
+
+var images = document.getElementsByClassName("zoomable");
+for (let i = 0; i < images.length; i++) {
+    images[i].onclick = function(){
+        modal.style.display = "block";
+        var modalImg = document.getElementById("modalImage");
+        var captionText = document.getElementById("caption");
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
